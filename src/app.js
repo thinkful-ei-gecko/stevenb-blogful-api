723ae4a7f3249ad90d038d884db4e80cf160faef
Steven Bull <stevenwbulljr@gmail.com>
2019-10-07 17:45:42 -0500

    implement commentsRouter in app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const articlesRouter = require('./articles/articles-router');
 const usersRouter = require('./users/users-router');
+const commentsRouter = require('./comments/comments-router');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 
 app.use('/api/articles', articlesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/comments', commentsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
